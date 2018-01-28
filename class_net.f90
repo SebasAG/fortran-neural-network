beaf1782d2c081c6b2b5cc9a97ed660855be4697
SebasAG <agsebastian@ciencias.unam.mx>
2018-01-28 14:25:07 -0600

    Added descriptions to class_net.f90 and started learning algorithm

--- class_net.f90
+++ class_net.f90
@@ -12,6 +12,10 @@ module class_net
 
 contains
   subroutine init_net(this, i, h, o)
+  !! initialize the network with
+  !!   'i' input nodes, 'h' hidden neurons and 'o' output nodes
+  !! to do: implement in the constructor function
+  !!   it seems gfortran doesn's support this at the time (?)
     class(net), intent(inout) :: this
     integer, intent(in) :: i, h, o
 
@@ -36,11 +40,30 @@ contains
   end subroutine init_net
 
   subroutine feed_forward(this, inputs)
+  !! feed forward algorithm
     class(net), intent(inout) :: this
     real, intent(in) :: inputs(this%i_size)
+    
+    !! weighted average of inputs plus bias
     this%h_z = matmul(this%h_w, inputs) + this%h_b
+    !! apply activation function (hidden layer)
     this%act = tanh(this%h_z)
+    !! result, with output weights plus bias
     this%res = matmul(this%o_w, this%act) + this%o_b
   end subroutine feed_forward
+
+  subroutine net_learn(this, inputs, expected)
+  !! learning algorithme from inputs and expected outputs
+    class(net), intent(inout) :: this
+    real, intent(in) :: inputs(this%i_size), expected(this%o_size)
+    real, dimension(this%o_size) :: error
+    integer :: i
+
+    call this%feedf(inputs)
+    error = expected - this%res
+
+    !! print error for debug
+    print '(*(f10.6))', (error(i), i = 1, this%o_size)
+  end subroutine net_learn
 end module class_net
 

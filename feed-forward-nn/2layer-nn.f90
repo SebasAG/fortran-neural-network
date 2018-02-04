ed9037671aab4440f09835ed156f03c2bab99426
SebasAG <agsebastian@ciencias.unam.mx>
2018-02-04 15:05:51 -0600

    Added training samples

--- feed-forward-nn/2layer-nn.f90
+++ feed-forward-nn/2layer-nn.f90
@@ -5,6 +5,10 @@ program neuron1
   integer, parameter :: i_size=2, h_size=2, o_size=1
   type(net) :: n1
 
+  integer, parameter :: samples = 10
+  real, dimension(samples) :: err2
+  integer :: i
+
   integer, dimension(:), allocatable :: seed
   integer :: seed_size
   call random_seed(size=seed_size)
@@ -13,13 +17,13 @@ program neuron1
 
   call n1%init(i_size, h_size, o_size, 0.1, seed=seed)
 
-  print *, n1%i_size, n1%h_size, n1%o_size
-  call n1%feedf((/1.0, 0.0/))
-  print *, n1%o_act(1)
-
-  print *, n1%learn((/1.0, 0.0/), (/1.0/))
-
-!  call save_net_dat
+  do i = 1, samples
+    err2(i) = n1%learn((/1.0,1.0/), (/0.0/)) &
+      + n1%learn((/1.0,0.0/), (/1.0/)) &
+      + n1%learn((/0.0,1.0/), (/1.0/)) &
+      + n1%learn((/0.0,0.0/), (/0.0/))
+    print *, err2(i)
+  enddo
 contains
   
   subroutine save_net_dat

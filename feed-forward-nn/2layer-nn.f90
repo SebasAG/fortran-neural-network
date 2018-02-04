e3d6a7c27cccbcff832caebd7865aa4809ca85d1
SebasAG <agsebastian@ciencias.unam.mx>
2018-02-04 15:12:19 -0600

    Added code to export error to file

--- feed-forward-nn/2layer-nn.f90
+++ feed-forward-nn/2layer-nn.f90
@@ -5,8 +5,8 @@ program neuron1
   integer, parameter :: i_size=2, h_size=2, o_size=1
   type(net) :: n1
 
-  integer, parameter :: samples = 10
-  real, dimension(samples) :: err2
+  integer, parameter :: samples = 100
+  real :: err2
   integer :: i
 
   integer, dimension(:), allocatable :: seed
@@ -17,12 +17,13 @@ program neuron1
 
   call n1%init(i_size, h_size, o_size, 0.1, seed=seed)
 
+  open(1, file="error.dat")
   do i = 1, samples
-    err2(i) = n1%learn((/1.0,1.0/), (/0.0/)) &
+    err2 = n1%learn((/1.0,1.0/), (/0.0/)) &
       + n1%learn((/1.0,0.0/), (/1.0/)) &
       + n1%learn((/0.0,1.0/), (/1.0/)) &
       + n1%learn((/0.0,0.0/), (/0.0/))
-    print *, err2(i)
+    write (1,*) err2
   enddo
 end program neuron1
 
